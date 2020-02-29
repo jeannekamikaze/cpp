30fa566f30bc96e007c1478f4034f10e5872a23c
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2020-02-28 23:04:32 -0800

    Clang format.

--- include/cpp/cpp.h
+++ include/cpp/cpp.h
@@ -8,55 +8,52 @@
 #endif
 
 /// A type that cannot be copied.
-struct NonCopyable
-{
-    NonCopyable () {}
+struct NonCopyable {
+  NonCopyable() {}
 
-    virtual ~NonCopyable () {}
+  virtual ~NonCopyable() {}
 
-    NonCopyable (const NonCopyable&) = delete;
-    NonCopyable& operator= (const NonCopyable&) = delete;
+  NonCopyable(const NonCopyable&) = delete;
+  NonCopyable& operator=(const NonCopyable&) = delete;
 };
 
 /// A type that cannot be moved.
-struct NonMovable
-{
-    NonMovable () {}
+struct NonMovable {
+  NonMovable() {}
 
-    virtual ~NonMovable () {}
+  virtual ~NonMovable() {}
 
-    NonMovable (NonMovable&&) = delete;
-    NonMovable& operator= (NonMovable&&) = delete;
+  NonMovable(NonMovable&&) = delete;
+  NonMovable& operator=(NonMovable&&) = delete;
 };
 
 /// A type that can neither be copied nor moved.
-struct unique : NonCopyable, NonMovable
-{
-    unique () {}
+struct unique : NonCopyable, NonMovable {
+  unique() {}
 
-    virtual ~unique () {}
+  virtual ~unique() {}
 };
 
 // Types
 
-#if defined(__GNUC__) || defined(__GNUG__) //Compiling with GNU C/C++ compiler
-    typedef __INT8_TYPE__   I8;
-    typedef __INT16_TYPE__  I16;
-    typedef __INT32_TYPE__  I32;
-    typedef __INT64_TYPE__  I64;
-    typedef __UINT8_TYPE__  U8;
-    typedef __UINT16_TYPE__ U16;
-    typedef __UINT32_TYPE__ U32;
-    typedef __UINT64_TYPE__ U64;
-#else //Visual Studio
-    typedef __int8  I8;
-    typedef __int16 I16;
-    typedef __int32 I32;
-    typedef __int64 I64;
-    typedef unsigned __int8  U8;
-    typedef unsigned __int16 U16;
-    typedef unsigned __int32 U32;
-    typedef unsigned __int64 U64;
+#if defined(__GNUC__) || defined(__GNUG__)  // Compiling with GNU C/C++ compiler
+typedef __INT8_TYPE__ I8;
+typedef __INT16_TYPE__ I16;
+typedef __INT32_TYPE__ I32;
+typedef __INT64_TYPE__ I64;
+typedef __UINT8_TYPE__ U8;
+typedef __UINT16_TYPE__ U16;
+typedef __UINT32_TYPE__ U32;
+typedef __UINT64_TYPE__ U64;
+#else  // Visual Studio
+typedef __int8 I8;
+typedef __int16 I16;
+typedef __int32 I32;
+typedef __int64 I64;
+typedef unsigned __int8 U8;
+typedef unsigned __int16 U16;
+typedef unsigned __int32 U32;
+typedef unsigned __int64 U64;
 #endif
 
 typedef unsigned uint;
--- src/Exception.cc
+++ src/Exception.cc
@@ -5,59 +5,61 @@
 
 #ifdef _MSC_VER
 #define snprintf _snprintf
-#pragma warning(disable: 4996)
+#pragma warning(disable : 4996)
 #endif
 
 #define EXP_SIZE 1024
 
 #ifdef _WIN32
-    #define TLS __declspec( thread )
+#define TLS __declspec(thread)
 #else
-    #define TLS __thread
+#define TLS __thread
 #endif
 
 TLS char exp_buf[EXP_SIZE];
 
 using namespace std;
 
-Exception::Exception (const char* what) throw () {
-    snprintf (exp_buf, EXP_SIZE, "%s", what);
+Exception::Exception(const char* what) throw() {
+  snprintf(exp_buf, EXP_SIZE, "%s", what);
 }
 
-Exception::Exception (const std::string& what) throw () {
-    snprintf (exp_buf, EXP_SIZE, "%s", what.c_str());
+Exception::Exception(const std::string& what) throw() {
+  snprintf(exp_buf, EXP_SIZE, "%s", what.c_str());
 }
 
-Exception::Exception (const std::ostringstream& what) throw () {
-    snprintf (exp_buf, EXP_SIZE, "%s", what.str().c_str());
-}
-
-#ifdef EXCEPTION_BOOST_FORMAT
-Exception::Exception(const boost::format& format) throw ()
-{
-    snprintf (exp_buf, EXP_SIZE, "%s", format.str().c_str());
-}
-#endif
-
-Exception::Exception (const char* file, int line, const char* what) throw () {
-    snprintf (exp_buf, EXP_SIZE, "File: %s, line: %d: %s", file, line, what);
-}
-
-Exception::Exception (const char* file, int line, const std::string& what) throw () {
-    snprintf (exp_buf, EXP_SIZE, "File: %s, line: %d: %s", file, line, what.c_str());
-}
-
-Exception::Exception (const char* file, int line, const std::ostringstream& what) throw () {
-    snprintf (exp_buf, EXP_SIZE, "File: %s, line: %d: %s", file, line, what.str().c_str());
+Exception::Exception(const std::ostringstream& what) throw() {
+  snprintf(exp_buf, EXP_SIZE, "%s", what.str().c_str());
 }
 
 #ifdef EXCEPTION_BOOST_FORMAT
-Exception::Exception(const char* file, int line, const boost::format& format) throw ()
-{
-    snprintf (exp_buf, EXP_SIZE, "File: %s, line: %d: %s", file, line, format.str().c_str());
+Exception::Exception(const boost::format& format) throw() {
+  snprintf(exp_buf, EXP_SIZE, "%s", format.str().c_str());
 }
 #endif
 
-const char* Exception::what () const throw () {
-    return exp_buf;
+Exception::Exception(const char* file, int line, const char* what) throw() {
+  snprintf(exp_buf, EXP_SIZE, "File: %s, line: %d: %s", file, line, what);
 }
+
+Exception::Exception(const char* file, int line,
+                     const std::string& what) throw() {
+  snprintf(exp_buf, EXP_SIZE, "File: %s, line: %d: %s", file, line,
+           what.c_str());
+}
+
+Exception::Exception(const char* file, int line,
+                     const std::ostringstream& what) throw() {
+  snprintf(exp_buf, EXP_SIZE, "File: %s, line: %d: %s", file, line,
+           what.str().c_str());
+}
+
+#ifdef EXCEPTION_BOOST_FORMAT
+Exception::Exception(const char* file, int line,
+                     const boost::format& format) throw() {
+  snprintf(exp_buf, EXP_SIZE, "File: %s, line: %d: %s", file, line,
+           format.str().c_str());
+}
+#endif
+
+const char* Exception::what() const throw() { return exp_buf; }
